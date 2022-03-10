a96ff1f87218e0dcfd7cbf0a397ad570e726c97e
Gasper Vrhovsek <gasper.vrhovsek@gmail.com>
2022-03-10 12:47:46 +0100

    INFRA-1410: remove rest, setup pom parent

--- src/test/java/id/global/iris/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package id.global.iris;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
