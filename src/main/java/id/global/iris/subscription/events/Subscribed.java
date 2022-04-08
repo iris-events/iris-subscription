8b84af520034bb4a684ce53955d69af3134ae2cb
Nejc Tomažič <nejc.tomazic@global.id>
2022-04-08 12:20:41 +0200

    INFRA-1410: bump iris version

--- src/main/java/id/global/iris/subscription/events/Subscribed.java
+++ src/main/java/id/global/iris/subscription/events/Subscribed.java
@@ -1,7 +1,7 @@
 package id.global.iris.subscription.events;
 
-import id.global.common.annotations.amqp.Message;
-import id.global.common.annotations.amqp.Scope;
+import id.global.common.annotations.iris.Message;
+import id.global.common.annotations.iris.Scope;
 
 @Message(name = "subscribed", scope = Scope.SESSION)
 public record Subscribed(String resourceType, String resourceId) {
