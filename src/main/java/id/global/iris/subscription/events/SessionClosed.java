3aab3f083832c7ff608185b92893b3b3812c2cf6
Tomaz Cerar <tomaz.cerar@gmail.com>
2023-02-23 20:53:34 +0100

    session closed is internal event

--- src/main/java/id/global/iris/subscription/events/SessionClosed.java
+++ src/main/java/id/global/iris/subscription/events/SessionClosed.java
@@ -3,6 +3,6 @@ package id.global.iris.subscription.events;
 import id.global.iris.common.annotations.Message;
 import id.global.iris.common.annotations.Scope;
 
-@Message(name = "session-closed", scope = Scope.FRONTEND)
+@Message(name = "session-closed", scope = Scope.INTERNAL)
 public record SessionClosed(String userId, String sessionId) {
 }
